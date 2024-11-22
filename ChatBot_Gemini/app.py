d278409786f9c88b7d9b0739d98aa4f59ce3797b
FranDevGame <fcoparjim@outlook.es>
2024-11-22 09:04:19 +0100

    Gemini-Chatbot

---
+++ ChatBot_Gemini/app.py
@@ -0,0 +1,33 @@
+from multiprocessing.pool import RUN
+from urllib import response
+import google.generativeai as genai
+import PIL.Image
+from properties import api_key, to_markdown
+
+
+def main():
+    # Config the API 
+    genai.configure(api_key=api_key)
+
+    # Establecemos el modelo que vamos a usar
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
+    # Ruta de la imagen al modelo
+    ruta_imagen = "remoto.jpg"
+    img = PIL.Image.open(ruta_imagen)
+
+    # # Send the image to the model
+    # response = model.generate_content(img)
+
+    # # Printer the response
+    # print(to_markdown(response.text))
+
+    instruction = "Escribe un breve y atractivo post de un blog basado en la imagen y en mi experiencia. Debe incluir una descripcion de la foto"
+
+    # To generate the content
+    response = model.generate_content([instruction, img], stream=True)
+    response.resolve()
+    print(to_markdown(response.text))
+
+if __name__ == "__main__":
+    main()
---
+++ ChatBot_Gemini/properties.py
@@ -0,0 +1,9 @@
+import textwrap
+
+api_key= ""
+
+
+def to_markdown(text):
+    text = text.replace("•", "  *")
+    return textwrap.indent(text, "-->", predicate=lambda _: True) # Predicate is a parameter that in itself is a function that decides whether a line should be indented or not; 
+    # with the lambda function whose parameter is _, and always True, we ensure that all lines (-->) will be identified at the beginning of each line break.
